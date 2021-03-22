4f6b127904b412441de3a200caa380adbe106643
Chandler-CYY <1453701762@qq.com>
2021-03-22 13:39:28 +0800

    初始化初始化应用应用

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll";
+
+export default {
+  name: "Scroll",
+  props: {
+    probeType: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      scroll: null,
+    };
+  },
+  mounted() {
+    // 1创建BScroll对象
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      click: true,
+      probeType: this.probeType,
+      pullUpLoad: this.pullUpLoad,
+    });
+    // 2监听滚动的位置
+    this.scroll.on("scroll", (position) => {
+      this.$emit("scroll", position);
+    });
+  },
+  methods: {
+    scrollTo(x, y, time = 300) {
+      this.scroll && this.scroll.scrollTo(x, y, time);
+    },
+    finishPullUp() {
+      this.scroll.finishPullUp();
+    },
+    refresh() {
+      this.scroll && this.scroll.refresh();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
